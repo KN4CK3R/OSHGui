006fe33ddc05c2fadf94afc1c428dda9ddb8b04e
KN4CK3R <KN4CK3R@ff2818e8-b839-42f7-b720-18d835743601>
2014-01-25 00:20:20 +0000

    -updated FreeType

--- branches/new_renderer/Dependencies/Source/FreeType/include/freetype/config/ftoption.h
+++ branches/new_renderer/Dependencies/Source/FreeType/include/freetype/config/ftoption.h
@@ -15,7 +15,6 @@
 /*                                                                         */
 /***************************************************************************/
 
-
 #ifndef __FTOPTION_H__
 #define __FTOPTION_H__
 
@@ -92,7 +91,7 @@ FT_BEGIN_HEADER
   /* This is done to allow FreeType clients to run unmodified, forcing     */
   /* them to display normal gray-level anti-aliased glyphs.                */
   /*                                                                       */
-/* #define FT_CONFIG_OPTION_SUBPIXEL_RENDERING */
+#define FT_CONFIG_OPTION_SUBPIXEL_RENDERING
 
 
   /*************************************************************************/
